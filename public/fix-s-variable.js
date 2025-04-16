37b67b60207d9ce8c63b1310e709b7ef8b023113
Fernando Nuno Vieira <f.nuno@D--F-MacBook-Pro.local>
2025-04-16 03:26:06 +0100

    Adicionado script para inicializar variável S preventivamente

---
+++ public/fix-s-variable.js
@@ -0,0 +1,3 @@
+// Define a variável 'S' globalmente para evitar o erro "Cannot access 'S' before initialization"
+window.S = window.S || {};
+console.log("Variável 'S' inicializada preventivamente");
