dc549f61fab3357cf1a00b37f9fbfe0a30b6a4b1
Fernando Nuno Vieira <f.nuno@D--F-MacBook-Pro.local>
2025-04-16 03:50:56 +0100

    Substituir script gptengineer.js com script personalizado

---
+++ public/blocked-script.js
@@ -0,0 +1,4 @@
+// Este arquivo substitui o script gptengineer.js
+// Definimos a variável S para evitar o erro "Cannot access 'S' before initialization"
+window.S = {};
+console.log("Script gptengineer.js substituído com sucesso");
